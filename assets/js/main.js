afab9eb797cc6335a2e657f5e95fa8de9a11d31c
Kristian Carter <carter.kristian@gmail.com>
2021-06-25 17:44:55 +0100

    Jekyll Build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,64 @@
-/**
- * Theming.
- *
- * Supports the preferred color scheme of the operation system as well as
- * the theme choice of the user.
- *
- */
-const themeToggle = document.querySelector(".theme-toggle");
-const chosenTheme = window.localStorage && window.localStorage.getItem("theme");
-const chosenThemeIsDark = chosenTheme == "dark";
-const chosenThemeIsLight = chosenTheme == "light";
+(function($) {
+  "use strict"; // Start of use strict
 
-// Detect the color scheme the operating system prefers.
-function detectOSColorTheme() {
-  if (chosenThemeIsDark) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else if (chosenThemeIsLight) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-  }
+  // Closes the sidebar menu
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    $(this).toggleClass("active");
+  });
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+})(jQuery); // End of use strict
+
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+  var that = $(this);
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
 }
-
-// Switch the theme.
-function switchTheme(e) {
-  if (chosenThemeIsDark) {
-    localStorage.setItem("theme", "light");
-  } else {
-    localStorage.setItem("theme", "dark");
-  }
-
-  detectOSColorTheme();
-  window.location.reload();
+var onMapClickHandler = function(event) {
+  var that = $(this);
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
 }
-
-// Event listener
-if (themeToggle) {
-  themeToggle.addEventListener("click", switchTheme, false);
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-  window
-    .matchMedia("(prefers-color-scheme: light)")
-    .addEventListener("change", (e) => e.matches && detectOSColorTheme());
-
-  detectOSColorTheme();
-} else {
-  localStorage.removeItem("theme");
-}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
